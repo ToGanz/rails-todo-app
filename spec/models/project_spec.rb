2964a4c68faadd977d34620a27e9382aa8bdb9c4
tganz <tganz5890@googlemail.com>
2021-04-27 06:14:56 +0200

    adds project validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Project.create(title: "June") }
+
+  context 'all required fields are present' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'name is missing' do
+    it 'is not valid' do
+      subject.update(title: '')
+      expect(subject.errors).to have_key(:title)
+    end
+  end
+
+	context 'name is not unique' do
+    before { Project.create(title: "June")}
+
+    it 'is not valid' do
+      expect(subject.errors[:title]).to include('has already been taken')
+    end
+  end
 end
