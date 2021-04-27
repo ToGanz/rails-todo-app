4f2e3a4c661fd760550ef5a02f8c310abcf0387d
tganz <tganz5890@googlemail.com>
2021-04-27 05:41:26 +0200

    add projects routes

--- spec/features/create_project_spec.rb
+++ spec/features/create_project_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a project', type: :feature do
   scenario 'valid inputs'  do
-    visit new_project_path
+    visit projects_path
     fill_in 'Title', with: 'Todos for Today'
     click_on 'Create Project'
     expect(page).to have_content('Project was successfully crrated.')
   end
 
   scenario 'invalid inputs' do
-    visit new_project_path
+    visit projects_path
     fill_in 'Title', with: ''
     click_on 'Create Project'
     expect(page).to have_content("Name can't be blank")
