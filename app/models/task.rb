9a51fdb7a8b48460b9d14bafaf1acb354e1db1a8
tganz <tganz5890@googlemail.com>
2021-04-29 04:55:23 +0200

    adds due date formatting

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   belongs_to :project
   validates :title, presence: true
   validates :project_id, presence: true
+
+  def formatted_date
+    if self.due_date
+      self.due_date.strftime("%d.%m.%Y")
+    end
+  end
 end
